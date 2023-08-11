c002779b1b5cc781fbbc14c0faf00df175ab58d1
Your Name <sumitkhan.dhana@gmail.com>
2023-08-11 10:00:48 +0530

    ake a branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import "../src/bijender.css";
-import '../src/ravi.css'
-import '../src/sumit.css'
-
+import "../src/ravi.css";
+import "../src/sumit.css";
+import WelcomeTo from "../src/components/WelcomeTo"
 function App() {
-  return <></>;
+  return (
+    <>
+      <WelcomeTo />
+    </>
+  );
 }
 
 export default App;
